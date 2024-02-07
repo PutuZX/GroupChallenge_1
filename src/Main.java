61214acb1a76be08fc4d3fcd747125a4abee3145
I Putu Bagus Widia Predana <78652589+PutuZX@users.noreply.github.com>
2024-02-07 09:11:15 +0700

    menu branch initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args)
     {
+        Main m = new Main();
+        m.menuPage();
         System.out.println("Hello world!");
+    }
+
+    public void menuPage(){
+        System.out.println("This is menu page");
     }
 }
