e87acc4ad0f07cf85a6768714f0cd3230013933e
I Putu Bagus Widia Predana <78652589+PutuZX@users.noreply.github.com>
2024-02-07 10:21:31 +0700

    finished namaGanjil

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,51 @@ public class Main {
         menuPage();
     }
 
+    public static void hitungGanjil() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan Kalimat : ");
+        boolean textFound = false;
+        String names = input.nextLine();
+        String name[] = names.split(" ");
+        for (int i = 0; i < name.length; i++) {
+            String huruf = name[i];
+            int penampung = (name[i].length());
+            if (penampung % 2 == 1) {
+                System.out.print(name[i] + " ");
+                textFound = true;
+            }
+        }
+        if(textFound){
+            reuseQuestion('4');
+        } else{
+            System.out.println("Text not found!");
+            reuseQuestion('4');
+        }
+    }
+
+    public static void reuseQuestion(char service){
+        Scanner input = new Scanner(System.in);
+        char answer;
+        System.out.println("\nPlease Type 'Y' to use again!");
+        char textInput = input.next().charAt(0);
+        answer = Character.toLowerCase(textInput);
+        if(answer == 'y'){
+            switch (service){
+                case '1':
+                    break;
+                case '2':
+                    break;
+                case '3':
+                    break;
+                case '4':
+                    hitungGanjil();
+                    break;
+            }
+        } else{
+            menuPage();
+        }
+    }
+
     public static void menuPage(){
         Scanner input = new Scanner(System.in);
         System.out.println("+----------------------------------------------------+");
@@ -26,8 +71,10 @@ public class Main {
             case '3':
                 break;
             case '4':
+                hitungGanjil();
                 break;
             case 'X':
+                System.out.println("\n========== Thank you for using our service! ==========");
                 break;
             default:
                 System.out.println("\n========== Wrong input! Please try again! =========\n");
